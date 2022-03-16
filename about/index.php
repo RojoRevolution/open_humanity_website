9b1d6511bc9aa77767fb5c119fecba9c89a3207d
David Rojo <rojo@rojorevolution.com>
2022-03-15 21:12:43 -0600

    added initial tags including the head and ending tags to includes

--- about/index.php
+++ about/index.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Open Humanity</title>
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="../assets/images/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="512x512" href="../assets/images/favicon/favicon-512x512.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="../assets/images/favicon/favicon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon/favicon-16x16.png">
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap" rel="stylesheet">
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- Main CSS Files -->
-    <link href="../assets/css/reset.css" rel="stylesheet">
-    <link href="../assets/css/main.css" rel="stylesheet">
-    <link href="../assets/css/mediaqueries.css" rel="stylesheet">
-</head>
-
-<body>
-    <?php include('../includes/mobile-menu.php') ?>
+<?php 
+$pageTitle = "About Open Humanity";
+$bodyID = "about";
+require('../includes/head.php') 
+?>
+    <?php require('../includes/mobile-menu.php') ?>
 
    <!-- Desktp Header -->
-    <?php include('../includes/main-header.php') ?>
+    <?php require('../includes/main-header.php') ?>
     <!-- Hero Area -->
     <section class="container-flex py-5">
         <div class="container padding-top-100">
@@ -73,8 +50,5 @@
             <a href="./sign-up.html"><button class="btn btn-lg rounded-pill primary-btn">Sign Up</button></a>
         </div>
     </section>
-    <?php include('../includes/footer.php') ?>
-    <?php include('../includes/scripts.php') ?>
-</body>
-
-</html>
+    <?php require('../includes/footer.php') ?>
+    <?php require('../includes/scripts.php') ?>
