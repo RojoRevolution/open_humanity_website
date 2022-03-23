38b39c6b46e9b9db5628b6246df5408ace60c4da
David Rojo <rojo@rojorevolution.com>
2022-03-22 20:25:55 -0500

    reverted back to html

--- includes/constant.php
+++
@@ -1,7 +0,0 @@
-<?php
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-$site_key = "6Ld2n78ZAAAAADV3O1fO45ve5LmDrpzMAA-lGKxR";
-$secret_key = "6Ld2n78ZAAAAAGoG0GBAv5FGm5YpWTDK9VoXzZN5";
-
-$contact_email = "rojo@rojorevolution.com";
-?>
